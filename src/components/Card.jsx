b3264c515e4e4baf3b5db02c8a1a4b1bdeb53007
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2023-06-11 13:31:02 -0400

    Card Fixes :hammer:

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,36 @@
 import styles from "./Card.module.css";
 import Button from "./Button";
 import { getMoviebyId } from "../services/loadAPI";
+import { useEffect, useState } from "react";
 
 export default function Card({ movie }) {
+  
+  const [moviedetails, setMoviedetails] = useState(null);
+
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const movieData = await getMoviebyId(movie.id);
+        setMoviedetails(movieData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchMovie();
+  }, []);
+
+  if (!moviedetails) {
+    return <div>Loading...</div>;
+  }
+
+  const genres = moviedetails.genres.map((genre) => genre.name).join(", ");
+  const languages = moviedetails.spoken_languages.map((language) => language.name).join(", ");
+
+
+  
+
+  
   return (
     <div className={styles.Card}>
       <h2 className={styles.Title}>{movie.title}</h2>
@@ -11,7 +39,8 @@ export default function Card({ movie }) {
         src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
         alt={movie.title}
       />
-      <span className={styles.Info}>gbuhrvbrbbevbriug</span>
+      <span className={styles.Info}>Géneros: {genres}</span>
+      <span className={styles.Info}>Lenguajes: {languages}</span>
       <Button
         href={`movieSpec/${movie.id}`}
         size="small"
